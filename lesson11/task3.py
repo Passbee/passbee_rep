ed8ec93c3fe625fefcd40be385e4695fd3faf637
Passbee <ilyaduddapas@gmail.com>
2020-03-30 11:16:30 +0300

    test

--- lesson11/task3.py
+++ lesson11/task3.py
@@ -1,24 +1,33 @@
 class Product:
-    def __init__(self, product_type, name, amount, price):
-        self.product_type = product_type
-        self.product_name = name
-        self.product_price = price
-        self.product_amount = amount
+    def __init__(self, product_type, name, price):
+        self.type = product_type
+        self.name = name
+        self.price = price
 
     def info(self):
-        print(f'{self.product_type}:\n{self.product_name}[Amount: {self.product_amount}|Price: {self.product_price}$]')
+        print(f'{self.type}:\n{self.name}|Price: {self.price}$]')
 
 
 class ProductStore:
-    def __init__(self, products):
-        self.products = products
+    tax = 3.6
+    stock = dict
 
-    def add_product(self, products):
-        self.products.append(products)
+    def __init__(self, *products):
+        self.stock = {product.name: [product.type, product.price * self.tax, 0] for product in products}
+
+    def add_product(self, product, quantity):
+        if product.name in self.stock:
+            self.stock[product.name][2] += quantity
+        else:
+            self.stock[product.name] = [product.type, product.price * self.tax, quantity]
 
 
 if __name__ == '__main__':
-    p1 = Product('drinks', 'Pepsi', 1, 1.50)
+    p1 = Product('drinks', 'Pepsi', 1.50)
+    p2 = Product('clothes', 'T-shirt', 12.45)
     p1.info()
-    s = ProductStore([])
-    s.add_product(p1)
+    s = ProductStore()
+    s.add_product(p1, 5)
+    s.add_product(p2, 3)
+    print(s.stock)
+
